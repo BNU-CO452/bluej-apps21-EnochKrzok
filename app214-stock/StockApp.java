10e58fe14729879809fe6347e011383c0e46d9a8
Hyp3rW1zz <91596611+Hyp3rW1zz@users.noreply.github.com>
2021-11-24 09:27:17 +0000

    initial start to app214

--- app214-stock/StockApp.java
+++ app214-stock/StockApp.java
@@ -4,14 +4,16 @@
  * stock manager so that users can add, edit,
  * print and remove stock products
  *
- * @author Student Name
- * @version 0.1
+ * @author Enoch Jozue Krzok
+ * @version 17/11/2021
  */
 public class StockApp
 {
     private InputReader reader;
     
-    //private ProductList stock;
+    private StockList stock;
+    
+    private StockDemo stockDemo;
     
     /**
      * Constructor for objects of class StockApp
@@ -20,8 +22,8 @@ public class StockApp
     {
         reader = new InputReader();
         
-        //stock = new ProductList();
-        //StockDemo demo = new StockDemo(stock);
+        stock = new StockList();
+        StockDemo demo = new StockDemo(stock);
     }
 
     /**
@@ -45,15 +47,78 @@ public class StockApp
     
     private boolean executeChoice(String choice)
     {
-        if(choice.equals("quit"))
+        if(choice.equals("quit") | choice.equals("q") )
         {
+            System.out.println("You have quit the app and it did not save");
             return true;
+        }
+        else if(choice.equals("add"))
+        {
+            int id = reader.getInt("Please enter ID of Product > ");
+            String name = reader.getString("Please enter name of Product > ");
+            Product product = new Product(id, name);
+            stock.add(product);
+            System.out.println("Product " +product.getName()+ " with ID " 
+                                +product.getID()+ " has been added");
+        }
+        else if(choice.equals("remove"))
+        {
+            int id = reader.getInt("Please enter ID of Product > ");
+            stock.remove(id);
+            System.out.println("Product with ID " +id+ " has been added");
         }
         else if(choice.equals("print"))
         {
-            //stock.print();
+            stock.print();
         }
-        
+        else if(choice.equals("buy"))
+        {
+            int id = reader.getInt("Please enter ID of Product > ");
+            int amount = reader.getInt("Please enter amount of Product to buy > ");
+            stock.buyProduct(id,amount);
+            System.out.println("you have brought Product with ID " +id+" "+amount+ " times");
+        }
+        else if(choice.equals("sell"))
+        {
+            int id = reader.getInt("Please enter ID of Product > ");
+            int amount = reader.getInt("Please enter amount of Product to sell > ");
+            stock.sellProduct(id,amount);
+            System.out.println("you have sold Product with ID " +id+" "+amount+ " times");
+        }
+        else if(choice.equals("search"))
+        {
+            String phrase = reader.getString("Please enter name of Product > ");
+            stock.findProducts(phrase);
+        }
+        else if(choice.equals("lowstock") | choice.equals("low stock"))
+        {
+            stock.lowStock();
+            String yorn = reader.getString("would you like to restock all (Y/N) > "); 
+            if(yorn.equals("y"))
+            {
+                stock.reStock();
+            }
+            else if(yorn.equals("n"))
+            {
+                System.out.println("You will go back to main menu");
+            }
+            else
+            {
+                System.out.println("Invalid input you will go back to main menu");
+            }
+        }
+        else if(choice.equals("re-stock"))
+        {
+            
+        }
+        else if(choice.equals("sd"))
+        {
+            StockDemo demo;
+        }
+        else
+        {
+            System.out.println("Invalid input try again");
+        }
         return false;
     }
    
@@ -65,7 +130,12 @@ public class StockApp
         System.out.println();
         System.out.println("    Add:        Add a new product");
         System.out.println("    Remove:     Remove an old product");
-        System.out.println("    Print:      Print all products");
+        System.out.println("    Search:     Search for products by what there name starts with, it will give you ID and quantity of product");
+        System.out.println("    Buy:        Buy an existing product by ID");
+        System.out.println("    Sell:       Sell an existing product by ID");
+        System.out.println("    Low Stock:   Print all products that have low stock, then have a option if you want to restock the low stock");
+        System.out.println("    Re-stock:   Restock the products that have lowstock");
+        System.out.println("    Print:      Print all the products, this will show the name, ID and quantity of all products");
         System.out.println("    Quit:       Quit the program");
         System.out.println();        
     }
@@ -77,7 +147,7 @@ public class StockApp
     {
         System.out.println("********************************");
         System.out.println("  App21-04: Stock Application ");
-        System.out.println("      by Student Name");
+        System.out.println("      by Enoch Jozue Krzok");
         System.out.println("********************************");
     }
 }
---
+++ app214-stock/StockDemo.java
@@ -0,0 +1,92 @@
+
+/**
+ * Demonstrate the StockManager and Product classes.
+ * The demonstration becomes properly functional as
+ * the StockManager class is completed.
+ * 
+ * @author David J. Barnes and Michael Kölling and Enoch Jozue Krzok
+ * @version 3/11/21
+ */
+public class StockDemo
+{
+    // The stock manager.
+    private StockList stock;
+
+    /**
+     * Create a StockManager and populate it with at least
+     * 10 sample products.
+     */
+    public StockDemo(StockList stock)
+    {
+        this.stock = stock;
+
+        // Add at least 10 products, they must be unique to you
+        // Make sure the ids are sequential numbers
+
+        stock.add(new Product(101, "Horizon Zero Dawn"));
+        stock.add(new Product(102, "Forza Horizon 5"));
+        stock.add(new Product(103, "League of Ledgends"));
+        stock.add(new Product(104, "CSGO"));
+        stock.add(new Product(105, "Hot Wheels Unleashed"));
+        stock.add(new Product(106, "Assassin's Creed Valhalla"));
+        stock.add(new Product(107, "Far Cry 6"));
+        stock.add(new Product(108, "Battlefield 2042"));
+        stock.add(new Product(109, "Test Drive Unlimited Solar Crown"));
+        stock.add(new Product(110, "Horizon Forbidden west"));
+        
+        runDemo();
+    }
+
+    /**
+     * Provide a demonstration of how the ProductList meets all
+     * the user requirements by making a delivery of each product 
+     * buying it in various amounts and then selling each
+     * product by various amounts. Make sure all the requirements
+     * have been demonstrated.
+     */
+    public void runDemo()
+    {
+        // Show details of all of the products before delivery.
+
+        stock.print();
+
+        buyProducts();
+        stock.print();        
+
+        sellProducts();
+        stock.print();        
+    }
+
+    /**
+     * buys stocks by ID with given amount.
+     */
+    private void buyProducts()
+    {
+        stock.buyProduct(101, 231);
+        stock.buyProduct(102, 1000);
+        stock.buyProduct(104);
+        stock.buyProduct(105, 738);
+        stock.buyProduct(106, 395);
+        stock.buyProduct(107, 51);
+        stock.buyProduct(108, 217);
+        stock.buyProduct(109, 111);
+        stock.buyProduct(110, 986);
+    }
+
+    /**
+     * sells stocks by ID with given amount.
+     */
+    private void sellProducts()
+    {
+        stock.sellProduct(101, 123);
+        stock.sellProduct(102, 768);
+        stock.sellProduct(103, 7);
+        stock.sellProduct(104, 17);
+        stock.sellProduct(105, 167);
+        stock.sellProduct(106, 337);
+        stock.sellProduct(107, 21);
+        stock.sellProduct(108, 127);
+        stock.sellProduct(109, 11);
+        stock.sellProduct(110, 896);
+    }    
+}
